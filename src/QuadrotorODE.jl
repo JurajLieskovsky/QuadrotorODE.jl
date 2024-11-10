5f52b7681a9f33cb227430fc2a06974f47d588fa
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-11-10 13:20:17 +0100

    init

---
+++ src/QuadrotorODE.jl
@@ -0,0 +1,179 @@
+module QuadrotorODE
+
+using LinearAlgebra
+using Parameters
+
+include("quaternions.jl")
+using .Quaternions
+
+# System's properties
+
+struct System
+    g::Vector # gravitation acceleration
+    m::Real   # mass
+    J::Matrix # moment of inertia
+    W::Matrix # moment matrix
+
+    function System(gravitation_accelaration, mass, moment_of_inertia, arm_length, torque_coefficient)
+        rotor_radius_vectors = [
+            [1, -1, 0],
+            [1, 1, 0],
+            [-1, 1, 0],
+            [-1, -1, 0]
+        ]
+
+        z_axis = [0, 0, 1]
+        direction_of_rotation = [1, -1, 1, -1]
+
+        moment_matrix = mapreduce(
+            arg -> arg[1] × z_axis * arm_length - arg[2] * z_axis * torque_coefficient,
+            hcat,
+            zip(rotor_radius_vectors, direction_of_rotation)
+        )
+
+        return new(gravitation_accelaration, mass, moment_of_inertia, moment_matrix)
+    end
+end
+
+# Dynamics (accelerations)
+
+"""
+Calculates the quadrotor's angular acceleration.
+
+arguments:
+    system - properties of the quadrotor
+    ω - angular velocity (in the frame of the quadrotor)
+    u - control inputs
+
+returns:
+    ω̇ - angular accelaration
+
+"""
+function angular_acceleration(system::System, ω, u)
+    @unpack J, W = system
+    return J \ (W * u - ω × (J * ω))
+end
+
+
+"""
+Calculates the quadrotor's linear acceleration.
+
+arguments:
+    system - properties of the quadrotor
+    q - orientation of the quadrotor (quaternion)
+    u - control inputs
+
+returns:
+    ω̇ - angular accelaration
+
+"""
+function linear_acceleration(system::System, q, u)
+    @unpack g, m = system
+    F = [0, 0, sum(u)]
+    return g + Quaternions.rot(q, F) / m
+end
+
+# State incrementation utility
+
+"""
+Calculates the system's state as incremented by dz.
+
+arguments:
+x₀ - system's state (x₀ = [r, q, v, ω])
+dz - incrementation of the state (dz = [dr, dθ, dv, dω]) 
+
+returns:
+x₀ + dx(dz) - incremented state (dx = [dr, q̇(q,dθ), dv, dω])
+
+"""
+function incremented_state(x₀, dz)
+    @assert length(x₀) == 13
+    @assert length(dz) == 12
+
+    r, q, v, ω = x₀[1:3], x₀[4:7], x₀[8:10], x₀[11:13]
+    dr, dθ, dv, dω = dz[1:3], dz[4:6], dz[7:9], dz[10:12]
+
+    return vcat(r + dr, q + Quaternions.q̇(q, dθ), v + dv, ω + dω)
+end
+
+# State difference utility
+
+"""
+Calculates the difference between the current and reference state in the tangential direction of the reference state.
+
+arguments:
+    x  - current state
+    x₀ - reference state
+
+returns:
+    dz - state difference (dz = [dr, dθ, dv, dω]) 
+    
+The approximation x ≈ x₀ + dx(dz), where dx = [dr, q̇(q,dθ), dv, dω], should be accurate for very small values of dθ
+    
+"""
+function state_difference(x, x₀)
+    @assert length(x) == 13
+    @assert length(x₀) == 13
+
+    dr = x[1:3] - x₀[1:3]
+    dθ = Quaternions.dθ(Quaternions.multiply(x[4:7], Quaternions.conjugate(x₀[4:7])))
+    dv = x[8:10] - x₀[8:10]
+    dω = x[11:13] - x₀[11:13]
+
+    return vcat(dr, dθ, dv, dω)
+end
+
+# State space descriptions
+
+"""
+Calculates the rate of change of the state according to the state description ẋ = f(x,u).
+
+arguments:
+system - properties of the quadrotor
+x - system's state (x = [r, q, v, ω])
+u - control inputs
+
+returns:
+ẋ - rate of change of the state (ẋ = [v, q̇, v̇, ω̇])
+
+"""
+function forward_dynamics(system, x, u)
+    @assert length(x) == 13
+    @assert length(u) == 4
+
+    _, q, v, ω = x[1:3], x[4:7], x[8:10], x[11:13]
+    ω̇ = angular_acceleration(system, ω, u)
+    v̇ = linear_acceleration(system, q, u)
+
+    return vcat(v, Quaternions.q̇(q, ω), v̇, ω̇)
+end
+
+
+"""
+Calculates the rate of change of the state in the tangential direction.
+
+arguments:
+system - properties of the quadrotor
+x₀ - system's state (x₀ = [r, q, v, ω])
+dz - increment of the state (dz = [dr, dθ, dv, dω]) 
+u  - control inputs
+
+returns:
+dż - rate of change of the state in tangential direction (dż = [v, ω, v̇, ω̇])
+
+"""
+function tangential_forward_dynamics(system, x₀, dz, u)
+    @assert length(x₀) == 13
+    @assert length(dz) == 12
+    @assert length(u) == 4
+
+    x = incremented_state(x₀, dz)
+    _, q, v, ω = x[1:3], x[4:7], x[8:10], x[11:13]
+
+    ω̇ = angular_acceleration(system, ω, u)
+    v̇ = linear_acceleration(system, q, u)
+
+    return vcat(v, ω, v̇, ω̇)
+end
+
+end
---
+++ src/quaternions.jl
@@ -0,0 +1,93 @@
+module Quaternions
+
+using LinearAlgebra
+
+"""
+Calculates the complex conjugate of a quaternion
+
+arguments:
+    q - quaternion
+
+returns:
+    q* - complex conjugate of q
+
+"""
+conjugate(q) = [q[1], -q[2], -q[3], -q[4]]
+
+
+"""
+Multiplies two quaternion p and q.
+
+arguments:
+    p - quaternion
+    q - quaternion
+
+returns:
+    pq - product of quaternion mulitiplication
+
+"""
+function multiply(p, q)
+    p₀, p⃗ = p[1], p[2:4]
+    q₀, q⃗ = q[1], q[2:4]
+    return vcat(p₀ * q₀ - p⃗'q⃗, p₀ * q⃗ + q₀ * p⃗ + p⃗ × q⃗)
+end
+
+
+"""
+Converts angular velocity to a quaternion's rate of change.
+
+arguments:
+    q - quaternion
+    ω - angular velocity (in the coordinates of q)
+
+returns:
+    q̇ - quaternion's rate of change
+
+"""
+function q̇(q, ω)
+    q₀, q⃗ = q[1], q[2:4]
+    return 0.5 * vcat(-q⃗'ω, q₀ * ω + q⃗ × ω)
+end
+
+
+"""
+Approximates a quaternion as a rotation about an arbitrary axis.
+
+arguments:
+    dq - quaternion
+
+returns:
+    dθ - angle * axis of rotation
+
+"""
+function dθ(dq)
+    dq₀, dq⃗ = dq[1], dq[2:4]
+    magnitude = norm(dq⃗)
+
+    if magnitude ≈ 0
+        return zeros(3)  # No rotation, return zero vector
+    end
+
+    θ = 2 * atan(magnitude / dq₀)
+    u = dq⃗ / magnitude
+    return θ * u
+end
+
+
+"""
+Transforms a 3D vector according to a unit quaternion.
+
+arguments:
+    q - unit quaternion
+    v - 3D vector
+
+returns:
+    v′ - transformed 3D vector
+
+"""
+function rot(q, v)
+    q₀, q⃗ = q[1], q[2:4]
+    return v + 2 * q⃗ × (q⃗ × v + q₀ * v)
+end
+
+end
